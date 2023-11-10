b277660edca66b7df4762bbf04b24f693ac079b7
Eric Lee <erlee92@yahoo.com>
2023-11-10 14:41:39 -0500

    rspec my_uniq

---
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd_problems.rb
@@ -0,0 +1,9 @@
+def my_uniq(arr)
+    new_arr = []
+
+    arr.each do |ele|
+        new_arr << ele if !new_arr.include?(ele)
+    end
+
+    new_arr
+end
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_problems_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_problems_spec.rb
@@ -3,21 +3,24 @@ require_relative '../lib/tdd_problems.rb'
 
 describe 'my_uniq' do
 
-    it "should take an Array as an argument" do
-        arr = [1, 3, 4, 5]
-        expect(my_uniq(arr)).to_not raise_error
+    # it "should take an Array as an argument" do
+    #     arr = [1, 3, 4, 5]
+    #     expect(my_uniq(arr)).to be([1,2])
+    # end
+
+    it "should return a new array with only unique elements from original array" do
+        arr = [1,1,2,2,3,3]
+
+        expect(my_uniq(arr)).to eq(arr.uniq)
+
     end
 
-    
+    it "should not use the built in uniq method" do 
+        arr = [1,1,2,3,3,]
 
-
-
-
-
-
-
-
-
-
+        expect(arr).to_not receive(:uniq)
+    end
 
 end
+
+describe
